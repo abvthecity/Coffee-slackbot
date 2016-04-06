b07a84534e649e15e0c9988be9a5911a18879a36
Andrew Jiang <andrewji@usc.edu>
2016-04-05 18:44:10 -0700

    testing env

--- bot.js
+++ bot.js
@@ -1,15 +1,24 @@
-var Bot = require('botkit');
-var redis = require('botkit/lib/storage/redis_storage');
-var http = require('http');
-var url = require('url');
+if (!process.env.TOKEN) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var Botkit = require('botkit');
+//var redis = require('botkit/lib/storage/redis_storage');
+//var http = require('http');
+//var url = require('url');
 
 var controller = Botkit.slackbot({
-	storage: redisStorage
+	debug:true
 });
 
 var bot = controller.spawn({
-	token: process.env.SLACK_TOKEN
+	token: process.env.TOKEN
 }).startRTM();
+
+controller.hears(['hello','hi','hey','help'],'direct_message,direct_mention,mention',function(bot,message){
+	bot.reply(message, "Hey whatsup?");
+});
 
 // SAVE command
 controller.hears(['save (.*)', 'save (.*) to (.*)'], 'direct_message,direct_mention,mention', function(bot, message){
@@ -34,8 +43,3 @@ controller.hears(['collections', 'show collections'], 'direct_message,direct_men
 controller.hears(['get (.*)','show (.*)', 'list (.*)', 'reading list (.*)'], 'direct_message,direct_mention,mention', function(bot, message){
 	bot.reply(message, "Sorry, nothing to get.");
 });
-
-http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Dyno is awake.');
-}).listesn(process.env.PORT || 5000);
