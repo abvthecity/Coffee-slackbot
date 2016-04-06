0473892ed0969ae68141da26c0da9cfd7e722283
Andrew Jiang <andrewji@usc.edu>
2016-04-05 18:57:34 -0700

    testing without infinite loop

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ var bot = controller.spawn({
 }).startRTM();
 
 controller.hears(['hello','hi','hey','help'],'direct_message,direct_mention,mention',function(bot,message){
-	bot.reply(message, "Hey whatsup?");
+	bot.reply(message, "Whatsup?");
 });
 
 // SAVE command
