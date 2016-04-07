f6b55ab0ce5f5a78bf081914e46ca1283ba72ea5
Andrew Jiang <andrewji@usc.edu>
2016-04-07 01:54:38 -0700

    beepboop-botkit

--- bot.js
+++ bot.js
@@ -1,67 +1,18 @@
 //require('dotenv').config();
 
-if (!process.env.clientId || !process.env.clientSecret || !process.env.port) {
-  console.log('Error: Specify clientId clientSecret and port in environment');
-  process.exit(1);
-}
-
 var Botkit = require('botkit');
+var BeepBoop = require('beepboop-botkit')
+
 var Firebase = require("firebase");
 var db = new Firebase("https://readcoffee.firebaseio.com/slackbot");
 
-var controller = Botkit.slackbot({
-  json_file_store: './db_slackbutton_bot/',
-}).configureSlackApp(
-  {
-    clientId: process.env.clientId,
-    clientSecret: process.env.clientSecret,
-    scopes: ['bot'],
-  }
-);
+var controller = Botkit.slackbot()
+var beepboop = BeepBoop.start(controller)
 
-controller.setupWebserver(process.env.port,function(err,webserver) {
-  controller.createWebhookEndpoints(controller.webserver);
-
-  controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
-    if (err) {
-      res.status(500).send('ERROR: ' + err);
-    } else {
-      res.send('Success!');
-    }
-  });
-});
-
-// just a simple way to make sure we don't
-// connect to the RTM twice for the same team
-var _bots = {};
-function trackBot(bot) {
-  _bots[bot.config.token] = bot;
-}
-
-controller.on('create_bot',function(bot,config) {
-
-  if (_bots[bot.config.token]) {
-    // already online! do nothing.
-  } else {
-    bot.startRTM(function(err) {
-
-      if (!err) {
-        trackBot(bot);
-      }
-
-      bot.startPrivateConversation({user: config.createdBy},function(err,convo) {
-        if (err) {
-          console.log(err);
-        } else {
-          convo.say('I am a bot that has just joined your team');
-          convo.say('You must now /invite me to a channel so that I can be of use!');
-        }
-      });
-
-    });
-  }
-
-});
+// listen for botkit controller events
+controller.on('bot_channel_join', function (bot, message) {
+  bot.reply(message, 'I\'m here!')
+})
 
 
 // Handle events related to the websocket connection to Slack
@@ -97,24 +48,3 @@ controller.hears(['random'],'direct_message,direct_mention,mention', command.ran
 
 // REMOVE command
 controller.hears(['remove (.*)', 'remove'],'direct_message,direct_mention,mention', command.remove);
-
-controller.storage.teams.all(function(err,teams) {
-
-  if (err) {
-    throw new Error(err);
-  }
-
-  // connect all teams with bots up to slack!
-  for (var t  in teams) {
-    if (teams[t].bot) {
-      controller.spawn(teams[t]).startRTM(function(err, bot) {
-        if (err) {
-          console.log('Error connecting bot to Slack:',err);
-        } else {
-          trackBot(bot);
-        }
-      });
-    }
-  }
-
-});
